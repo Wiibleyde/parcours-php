a625b4819364927eb44bcc4bd4377ae7385d79c9
Wiibleyde <nathan@bonnell.fr>
2023-11-23 10:59:31 +0100

    Refactor function signatures to include return types
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- reverser.php
+++ reverser.php
@@ -1,9 +1,9 @@
 <?php
-function reverse($string) {
+function reverse($string): string {
     return strrev($string);
 }
 
-function isPalindrome($string) {
+function isPalindrome($string): bool {
     return $string == reverse($string);
 }
 ?>
