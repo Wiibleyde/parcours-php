fde868fb53a1086db052af8a20f3ef987cb596dd
Wiibleyde <nathan@bonnell.fr>
2023-11-23 10:34:01 +0100

    Update function return types
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- say-hello-to-functions.php
+++ say-hello-to-functions.php
@@ -1,9 +1,9 @@
 <?php
-function sayHello() {
+function sayHello(): string {
     return "Hello";
 }
 
-function sayHelloTo($name) {
+function sayHelloTo($name): string {
     return "Hello " . $name;
 }
 ?>
