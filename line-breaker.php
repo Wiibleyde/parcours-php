c6faaa2ddd5aa5a694cb9cb3126f6007babee352
Wiibleyde <nathan@bonnell.fr>
2023-11-23 11:14:39 +0100

    Fix bug in login functionality
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ line-breaker.php
@@ -0,0 +1,24 @@
+<?php
+
+function breakLines($string, $maxLength) {
+    $result = '';
+
+    while ($string != '') {
+        if (strlen($string) <= $maxLength) {
+            $result .= $string . "\n";
+            break;
+        } else {
+            $lastSpace = strrpos(substr($string, 0, $maxLength), ' ');
+            if ($lastSpace === false) {
+                throw new Exception('Word length exceeds maximum line length');
+            }
+            $result .= substr($string, 0, $lastSpace) . "\n";
+            $string = substr($string, $lastSpace + 1);
+        }
+    }
+
+    return $result;
+}
+
+echo breakLines("Line with words should break", 15);
+?>
