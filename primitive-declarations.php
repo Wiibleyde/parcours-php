98f60b5f23f581dc5910b6b276d139a753fae789
Wiibleyde <nathan@bonnell.fr>
2023-11-23 09:33:38 +0100

    Fix bug in login functionality
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php
+$firstStr = "Hello World";
+$firstEscapeStr = "Hello World \n";
+$firstBool = true;
+$firstInteger = 1;
+$firstFloat = 1.5;
+$firstNull = NULL;
+$firstArray = array(2, NULL, "Hi", 5.5, false);
+?>
