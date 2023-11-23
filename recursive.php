02765d02560c5f324870a27639dbf2d55c982636
Wiibleyde <nathan@bonnell.fr>
2023-11-23 16:34:05 +0100

    Fix factorial function recursion bug
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- recursive.php
+++ recursive.php
@@ -1,11 +1,11 @@
 <?php
+
 function factorial(int $number): int {
     if ($number <= 1) {
         return 1;
     } else {
-        return (int)($number * factorial($number - 1));
+        return $number * factorial($number - 1);
     }
 }
-echo factorial(5);
 
 ?>
