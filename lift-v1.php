c11e220e002880742938deee727f4921cc4bc72d
Wiibleyde <nathan@bonnell.fr>
2023-11-23 15:40:03 +0100

    Refactor getFloor and getDirection functions
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- lift-v1.php
+++ lift-v1.php
@@ -1,26 +1,38 @@
 <?php
-
-function getFloor(int $currentFloor, ?int $requestedFloor, array $calledFloors): int {
-    if ($requestedFloor !== null) {
-        return $requestedFloor;
-    }
-    $closestFloor = 0;
-    foreach ($calledFloors as $floor) {
-        if ($closestFloor === null || abs($currentFloor - $floor) < abs($currentFloor - $closestFloor)) {
-            $closestFloor = $floor;
+function getFloor(int $currentFloor, ?int $requestFloor, array $listButtonCalled):  null|int {
+    if (is_int($requestFloor)) {
+        return $requestFloor;
+    } else {
+        $i = true;
+        $scale = 0;
+        while ($i == true) {
+            if (in_array($currentFloor,$listButtonCalled)){
+                return $currentFloor;
+            }
+            if (count($listButtonCalled) == 0){
+                return null;
+            }
+            if (in_array($currentFloor + $scale, $listButtonCalled)) {
+                return $currentFloor + $scale;
+            } elseif (in_array($currentFloor - $scale, $listButtonCalled)) {
+                return $currentFloor - $scale;
+            }
+            $scale++;
         }
     }
-    return $closestFloor;
 }
 
-function getDirection(int $currentFloor, ?int $requestedFloor, array $calledFloors): int {
-    $nextFloor = getFloor($currentFloor, $requestedFloor, $calledFloors);
-    if ($nextFloor == $currentFloor) {
-        return 0;
-    } elseif ($nextFloor > $currentFloor) {
-        return 1;
+function getDirection(int $currentFloor, ?int $requestFloor): int {
+    if (is_int($requestFloor)) {
+        if ($currentFloor < $requestFloor) {
+            return 1;
+        } elseif ($currentFloor > $requestFloor) {
+            return -1;
+        } else {
+            return 0;
+        }
     } else {
-        return -1;
+        return 0;
     }
 }
 ?>
