566404e113f0a47a98bf5a72736a51a337de410a
Wiibleyde <nathan@bonnell.fr>
2023-12-04 10:56:50 +0100

    Add validation for minimum number of arrays in myArrayMap function
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,9 +1,13 @@
 <?php
 
 function myArrayMap(callable $callback, array ...$arrays): array {
-    $result = [];
+    if (count($arrays) < 2) {
+        throw new InvalidArgumentException('myArrayMap expects at least two arrays');
+    }
 
+    $result = [];
     $length = count($arrays[0]);
+
     for ($i = 0; $i < $length; $i++) {
         $params = array_column($arrays, $i);
         $result[] = $callback(...$params);
