c2995f61e6c9794c4d251505283e752981e322e3
Wiibleyde <nathan@bonnell.fr>
2023-12-04 09:26:05 +0100

    Fix bug in login functionality
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ thats-anonymous.php
@@ -0,0 +1,11 @@
+<?php
+$today = function() {
+    return "It is " . date("F j, Y");
+};
+
+$isLeapYear = function($year) {
+    return ($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0);
+};
+
+echo $today();
+?>
