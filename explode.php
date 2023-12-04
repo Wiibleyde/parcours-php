caa754483131acdac887221b4a670ede8b994710
Wiibleyde <nathan@bonnell.fr>
2023-12-04 09:54:47 +0100

    Refactor explodeWords function to handle limit parameter correctly
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- explode.php
+++ explode.php
@@ -3,20 +3,24 @@
 function explodeWords(string $str, string $separator = ' ', int $limit = PHP_INT_MAX): array {
     $result = [];
     $word = '';
-    $i = 0;
-    while ($i < strlen($str) && count($result) < $limit) {
-        if ($str[$i] === $separator) {
-            $result[] = $word;
-            $word = '';
-        } else {
+    $len = strlen($str);
+    for ($i = 0; $i < $len; $i++) {
+        if ($str[$i] != $separator || $i == $len - 1) {
             $word .= $str[$i];
         }
-        $i++;
+        if ($str[$i] == $separator || $i == $len - 1) {
+            $result[] = $word;
+            $word = '';
+            if (count($result) == $limit) {
+                break;
+            }
+        }
     }
-    $result[] = $word;
+    if ($limit < 0) {
+        $result = array_slice($result, 0, $limit);
+    } elseif ($limit == 0 && count($result) > 0) {
+        $result = [$result[0]];
+    }
     return $result;
 }
-
-echo explodeWords('My-name/is-John', '/'); // ['My-name', 'is-John']
-
 ?>
