c60b5a3d15795e3dc62cf87e1356584a7ce67f14
Wiibleyde <nathan@bonnell.fr>
2023-12-04 10:53:03 +0100

    Refactor myArrayMap to accept multiple arrays as parameters
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,10 +1,12 @@
 <?php
 
-function myArrayMap(callable $callback, array $array): array {
+function myArrayMap(callable $callback, array ...$arrays): array {
     $result = [];
 
-    foreach ($array as $value) {
-        $result[] = $callback($value);
+    $length = count($arrays[0]);
+    for ($i = 0; $i < $length; $i++) {
+        $params = array_column($arrays, $i);
+        $result[] = $callback(...$params);
     }
 
     return $result;
