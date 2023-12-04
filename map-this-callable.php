3db3de5f1aa7e8094abccc3b2a43c327bfc2ca47
Wiibleyde <nathan@bonnell.fr>
2023-12-04 11:32:20 +0100

    Refactor array mapping logic in myArrayMap function
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- map-this-callable.php
+++ map-this-callable.php
@@ -4,10 +4,15 @@ function myArrayMap(callable $callback, array $array, array ...$arrays): array {
     $result = [];
     $arrays = array_merge([$array], $arrays);
     $count = count($arrays[0]);
+
     for ($i = 0; $i < $count; $i++) {
-        $args = array_map(fn($array) => $array[$i], $arrays);
+        $args = [];
+        foreach ($arrays as $arr) {
+            $args[] = $arr[$i];
+        }
         $result[] = $callback(...$args);
     }
+
     return $result;
 }
 
