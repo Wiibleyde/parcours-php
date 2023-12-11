1239cd0796a53c948cfd1de13d1d04b7aa1407c1
Wiibleyde <nathan@bonnell.fr>
2023-12-11 10:03:53 +0100

    Add constructor to Mojito class
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- i-want-a-cocktail.php
+++ i-want-a-cocktail.php
@@ -3,5 +3,11 @@ class Mojito {
     public float $alcoholRate = 0.15;
     public array $ingredients = ['rum', 'lime', 'sparkling water', 'mint', 'sugar'];
     public int $price = 8;
+
+    public function __construct(float $alcoholRate, array $ingredients, int $price) {
+        $this->alcoholRate = $alcoholRate;
+        $this->ingredients = $ingredients;
+        $this->price = $price;
+    }
 }
 ?>
